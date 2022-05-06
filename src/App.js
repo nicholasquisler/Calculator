dfe74aff86fae761a18147f516a3e5aff0a68c6c
nicholasquisler <nquisler@gmail.com>
2022-05-06 03:46:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ItemList from './itemList';
+import { numbers, operators, equals, clear, sin } from './numbers';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [answer, setAnswer] = useState();
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item) => {
+    setCart(oldCart => [...oldCart, item]);
+  };
+
+  const removeItem = (targetIndex) => {
+    setCart(oldCart => oldCart.filter((item, index) => index !== targetIndex));
+  }
+
+  const doEquals = () => {
+    setAnswer(eval(cart.join('')));
+    setCart([eval(cart.join(''))]);
+  }
+
+  const doClear = () => {
+    setCart([]);
+    setAnswer([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Basic Calculator Web App</h1>
+      <h1 className="answer">{answer}</h1>
+      <ul className="preMath">
+        {cart.map((item, index) => (
+          <li onClick={() => removeItem(index)} key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <div className="buttons">
+        <div className="numbers">
+          <ItemList items={numbers} onItemClick={addItem} />
+        </div>
+        <div className="operators">
+          <ItemList items={operators} onItemClick={addItem} />
+        </div>
+      </div>
+      <ItemList items={equals} onItemClick={doEquals} />
+      <ItemList items={clear} onItemClick={doClear} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/numbers.js
@@ -0,0 +1,25 @@
+export const numbers = [
+    7,
+    8,
+    9,
+    4,
+    5,
+    6,
+    1,
+    2,
+    3,
+    ".",
+    0,
+    "-"
+];
+
+export const operators = [
+    "/",
+    "*",
+    "-",
+    "+"
+];
+
+export const equals = ["="];
+
+export const clear = ["CE"];
